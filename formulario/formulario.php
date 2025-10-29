7bcf84a55a91ad2e0476f9042244ca233e86774e
guigarher <guigarher@gmail.com>
2025-10-29 13:48:20 +0000

    asd

--- formulario/formulario.php
+++ formulario/formulario.php
@@ -6,21 +6,23 @@
   <script src="formulario.js" defer></script>
 </head>
 <body>
+  <?php date_default_timezone_set('Atlantic/Canary'); ?>
+
   <h1>Formulario</h1>
 
   <form action="respuesta.php" method="post">
     <label>Nombre:
-      <input type="text" name="nombre" required>
+      <input type="text" name="nombre" id="nombre" required>
     </label>
     <br><br>
 
     <label>Apellido:
-      <input type="text" name="apellido" required>
+      <input type="text" name="apellido" id="apellido" required>
     </label>
     <br><br>
 
     <label>E-mail:
-      <input type="email" name="email" required>
+      <input type="email" name="email" id="email" required>
     </label>
     <br><br>
 
@@ -28,8 +30,13 @@
       <input type="number" name="veces" min="1" value="1" required>
     </label>
     <br><br>
+    <label>Aceto las condiciones
+    <input type="checkbox" name="check" id="check" required>
+    </label>
+    <br><br>
+      <input type="hidden" name="fecha" id="fecha" value= "<?php echo date('l-d-m-Y h:i:sa'); ?>">
 
-    <button type="submit">Enviar</button>
+    <button type="submit" id="boton">Enviar</button>
   </form>
 </body>
 </html>
